516eafe939b7294aeeef476b5ae3fc4ddc75f365
Huy Mạc Quang <57699454+huymq1710@users.noreply.github.com>
2025-08-31 21:52:54 +0700

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import App from './App.tsx'
 // import TestApp from './TestApp.tsx'
 import './index.css'
 
-// Import test Firebase để debug
-import './test-firebase.ts'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
